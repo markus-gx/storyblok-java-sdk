b7e8e12d0e4d006cf4923caf6373539bc514b400
Markus Geilehner <markus@geilehner.io>
2021-04-08 16:32:44 +0200

    Added Color Field Type; Bugfix

--- src/test/java/io/geilehner/storyblok/model/CustomStoryContent.java
+++ src/test/java/io/geilehner/storyblok/model/CustomStoryContent.java
@@ -3,13 +3,4 @@ package io.geilehner.storyblok.model;
 import io.geilehner.storyblok.model.content.StoryContent;
 
 public class CustomStoryContent extends StoryContent {
-    private String _uid;
-
-    public void set_uid(String _uid) {
-        this._uid = _uid;
-    }
-
-    public String get_uid() {
-        return _uid;
-    }
 }
